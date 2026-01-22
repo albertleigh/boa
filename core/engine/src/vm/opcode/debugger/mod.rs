b2e50dd5c3b3162a80b225826c6843640fc80534
Albert Li <wentaoli@microsoft.com>
2026-01-23 02:20:08 +0800

    let's support debugger try30

--- core/engine/src/vm/opcode/debugger/mod.rs
+++ core/engine/src/vm/opcode/debugger/mod.rs
@@ -14,19 +14,20 @@ pub(crate) struct Debugger;
 
 impl Debugger {
     #[inline(always)]
+    #[cfg(feature = "debugger")]
     pub(crate) fn operation((): (), context: &mut Context) -> ControlFlow<CompletionRecord> {
         // Call the debugger hook from the host hooks
-
-        #[cfg(feature = "debugger")]
         match context.host_hooks().on_debugger_statement(context) {
             Ok(()) => ControlFlow::Continue(()),
             Err(err) => context.handle_error(err),
         }
+    }
 
-        #[cfg(not(feature = "debugger"))]
-        {
-            ControlFlow::Continue(())
-        }
+    #[inline(always)]
+    #[cfg(not(feature = "debugger"))]
+    pub(crate) fn operation((): (), _context: &mut Context) -> ControlFlow<CompletionRecord> {
+        // Call the debugger hook from the host hooks
+        ControlFlow::Continue(())
     }
 }
 
